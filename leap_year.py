f86852aae77af23b60a782e890902f4bd727bd97
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-10-31 16:56:11 +0000

    Added leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -14,16 +14,16 @@ def main():
         # convert user age to an integer
         user_year_int = int(user_year_string)
         # if the user is less than 0 or greater than 120, they are not a valid age
-        
+
         if user_year_int % 4 == 0:
             if user_year_int % 100 == 0:
                 if user_year_int % 400 == 0:
-                    print ("It is a leap year")
-                else : 
-                    print ("It is not a leap year")
+                    print("It is a leap year")
+                else:
+                    print("It is not a leap year")
             else:
-                print ("It is a leap year")
-        else : 
+                print("It is a leap year")
+        else:
             print("It is not a leap year.")
 
     except:
